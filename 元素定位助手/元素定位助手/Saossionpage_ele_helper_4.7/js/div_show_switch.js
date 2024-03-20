a97ff33f2bc632ac222b0be7fd2b29aeb6ccd41f
Your Name <you@example.com>
2024-03-20 22:04:43 +0800

    弹出增加 开关按钮

--- 元素定位助手/元素定位助手/Saossionpage_ele_helper_4.7/js/div_show_switch.js
+++ 元素定位助手/元素定位助手/Saossionpage_ele_helper_4.7/js/div_show_switch.js
@@ -5,9 +5,9 @@ function set_on() {
 
     // 读取数据
     chrome.storage.local.get('show_div', function (result) {
-        console.log('显示div is ' + result.show_div);
+        // console.log('显示div is ' + result.show_div);
         document.getElementById('ceshi').innerHTML = '->' + result.show_div;
-        console.log(result.show_div);
+        // console.log(result.show_div);
 
     });
 
@@ -19,14 +19,25 @@ function set_off() {
 
     // 读取数据
     chrome.storage.local.get('show_div', function (result) {
-        console.log('显示div is ' + result.show_div);
+        // console.log('显示div is ' + result.show_div);
         document.getElementById('ceshi').innerHTML = '->' + result.show_div;
-        console.log(result.show_div);
+        // console.log(result.show_div);
 
     });
 
 }
 
+function toggleSwitch() {
+    // 切换开关状态
+    var checkBox = document.getElementById("kaiguan1");
+    if (checkBox.checked) {        
+        
+        set_off();
+    } else {       
+        
+        set_on();
+    }
+}
 
 
 // 获取 on1 按钮元素并添加点击事件监听器
@@ -35,6 +46,7 @@ console.log(document.URL);
 
 document.getElementById('on1').addEventListener('click', set_on);
 document.getElementById('off1').addEventListener('click', set_off);
+document.getElementById("kaiguan1").addEventListener("click", toggleSwitch);
 // 默认展示信息栏
 set_on();
 
